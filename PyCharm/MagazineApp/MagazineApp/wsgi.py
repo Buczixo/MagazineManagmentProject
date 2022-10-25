49f19217c4f7d66b44bcc5e12353503154450eb2
Maciej Buczak <97064870+Buczixo@users.noreply.github.com>
2022-10-25 20:17:25 +0200

    project restart with venv

--- PyCharm/MagazineApp/MagazineApp/wsgi.py
+++ PyCharm/MagazineApp/MagazineApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PyCharmFiles project.
+WSGI config for MagazineApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyCharmFiles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MagazineApp.settings')
 
 application = get_wsgi_application()
