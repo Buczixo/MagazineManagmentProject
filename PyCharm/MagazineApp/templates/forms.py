fc004866219f4aaf82bbefd7611d337c075f50ca
Buczixo <97064870+Buczixo@users.noreply.github.com>
2022-11-15 16:51:06 +0100

    w pół działająca rejestracja (nie wyświetla błędów)

---
+++ PyCharm/MagazineApp/templates/forms.py
@@ -0,0 +1,21 @@
+# import formularza kreacji
+from django.contrib.auth.forms import UserCreationForm
+
+# import modeli formularzy
+from django import forms
+
+# import modeli użytkownika
+from django.contrib.auth.models import User
+
+# edycja formularza kreacji żeby był własny tekst
+class ForDodPrac(UserCreationForm):
+    class Meta:
+        model = User
+        # https://docs.djangoproject.com/en/1.10/topics/forms/modelforms/#overriding-the-default-fields
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Nazwa Użytkownika :',
+            'email': 'Email służbowy :',
+        }
+        password1 = forms.CharField(label="Podaj hasło :")
+        password2 = forms.CharField(label="Potwierdź hasło :")
