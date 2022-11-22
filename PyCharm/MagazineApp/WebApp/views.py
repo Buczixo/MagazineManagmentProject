055d4abba1b006c8e63ddcbe1326db4886ba60d8
Buczixo <97064870+Buczixo@users.noreply.github.com>
2022-11-22 19:11:35 +0100

    logowanie skończone

--- PyCharm/MagazineApp/WebApp/views.py
+++ PyCharm/MagazineApp/WebApp/views.py
@@ -6,72 +6,104 @@ from django.contrib import messages
 # https://www.youtube.com/watch?v=tUqUdu0Sjyc
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-#import zmienionego formularza
+# import zmienionego formularza
 from templates.forms import ForDodPrac
+
+# import biblioteki restrykcji otwierania
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def rejestracja(request):
-    form = ForDodPrac()
-    context = {'form':form}
-    # Zapis danych z formularza jeśli formularz jest poprawny oraz auto hashowanie hasła
-    if request.method == 'POST':
-        form = ForDodPrac(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Użytkownik %s utworzony poprawnie' % user)
-            return redirect('logowanie')
-
+    if request.user.is_authenticated :
+        return redirect('glowna')
+    else :
+        form = ForDodPrac()
+        context = {'form': form}
+        # Zapis danych z formularza jeśli formularz jest poprawny oraz auto hashowanie hasła
+        if request.method == 'POST':
+            form = ForDodPrac(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Użytkownik %s utworzony poprawnie' % user)
+                return redirect('logowanie')
 
     return render(request, 'rejestracja.html', context)
+
+
+@login_required(login_url='logowanie')
 def main(request):
     return render(request, 'index.html')
+
+
 def logowanie(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated :
+        return redirect('home')
+    else :
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, "Hasło albo login niepoprawne !")
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, "Hasło albo login niepoprawne !")
 
-    context = {}
-    return render(request, 'logowanie.html')
+        context = {}
+        return render(request, 'logowanie.html')
 
+
+@login_required(login_url='logowanie')
 def wylogowanie(request):
     logout(request)
     return redirect('logowanie')
 
-def odbior(request) :
+
+@login_required(login_url='logowanie')
+def odbior(request):
     return render(request, 'odbior.html')
 
-def wydanie(request) :
+
+@login_required(login_url='logowanie')
+def wydanie(request):
     return render(request, 'wydanie.html')
 
-def przerzucenie(request) :
+
+@login_required(login_url='logowanie')
+def przerzucenie(request):
     return render(request, 'przerzucenie.html')
 
-def u_magazynu(request) :
+
+@login_required(login_url='logowanie')
+def u_magazynu(request):
     return render(request, 'u_magazynu.html')
 
-def w_magazynu(request) :
+
+@login_required(login_url='logowanie')
+def w_magazynu(request):
     return render(request, 'w_magazynu.html')
 
-def wyszukanie(request) :
+
+@login_required(login_url='logowanie')
+def wyszukanie(request):
     return render(request, 'wyszukanie.html')
 
-def b_towarow(request) :
+
+@login_required(login_url='logowanie')
+def b_towarow(request):
     return render(request, 'b_towarow.html')
 
-def b_dostawcow(request) :
+
+@login_required(login_url='logowanie')
+def b_dostawcow(request):
     return render(request, 'b_dostawcow.html')
 
-def b_pracownikow(request) :
+
+@login_required(login_url='logowanie')
+def b_pracownikow(request):
     return render(request, 'b_pracownikow.html')
-
-
