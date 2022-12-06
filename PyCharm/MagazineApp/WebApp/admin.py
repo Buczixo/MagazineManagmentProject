bd5b40346d495e19bc4d93884707ea823c0b7fbf
Buczixo <97064870+Buczixo@users.noreply.github.com>
2022-12-06 17:11:10 +0100

    mag models work

--- PyCharm/MagazineApp/WebApp/admin.py
+++ PyCharm/MagazineApp/WebApp/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Magazyn
+from .models import Magazyn, Produkt, Opakowanie
 
-
+# https://docs.djangoproject.com/en/4.1/topics/db/queries/
+# https://books.agiliq.com/projects/django-admin-cookbook/en/latest/database_view.html
 # Register your models here.
 @admin.register(Magazyn)
-class AllEntiryAdmin(admin.ModelAdmin):
-    list_display = ("idMagazynu", "idMagazynu")
+class AdmMag (admin.ModelAdmin):
+    list_display = ("idMagazynu", "nrOpakowania")
+
+@admin.register(Produkt)
+class AdmPro (admin.ModelAdmin):
+    list_display = ("nazwaProduktu", "cenaBrutto")
+
+@admin.register(Opakowanie)
+class AdmOpa (admin.ModelAdmin):
+    list_display = ("idPudelka", "towar")
--- PyCharm/MagazineApp/WebApp/models.py
+++ PyCharm/MagazineApp/WebApp/models.py
@@ -2,30 +2,43 @@ from django.db import models
 
 # Create your models here.
 
+# https://docs.djangoproject.com/en/4.1/topics/db/examples/many_to_one/
 
 VAT_CHOICES = [
     ('A', '23%'),
     ('B', '5%'),
     ('C', '0%'),
-    ('d', 'Nie dotyczy'),
+    ('D', 'Nie dotyczy'),
+]
+TYPE_OF_ILOSC = [
+    ('KG', 'kg'),
+    ('SZT', 'szt.'),
 ]
 
 
 class Produkt(models.Model):
-    nazwaProduktu = models.CharField(max_length=20, default=' ')
-    cenaBrutto = models.DecimalField(max_digits=10, decimal_places=2)
-    cenaNetto = models.DecimalField(max_digits=10, decimal_places=2)
-    stawkaVat = models.CharField(max_length=3, choices=VAT_CHOICES, default=' ')
-    typIlosci = models.CharField(max_length=20, default=' ')
+    nazwaProduktu = models.CharField(max_length=20, default=' ', primary_key=True)
+    cenaBrutto = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    cenaNetto = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    stawkaVat = models.CharField(max_length=5, choices=VAT_CHOICES, default=' ')
+    typIlosci = models.CharField(max_length=5, choices=TYPE_OF_ILOSC, default=' ')
 
     def __str__(self):
         return self.nazwaProduktu
 
 
-class Opakowanie(Produkt):
-    idPudelka = models.IntegerField(default=0)
-    iloscProduktu = models.DecimalField(max_digits=10, decimal_places=2)
+class Opakowanie(models.Model):
+    idPudelka = models.IntegerField(default=0, primary_key=True)
+    iloscProduktu = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    towar = models.ForeignKey(Produkt, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.idPudelka
 
 
-class Magazyn(Opakowanie):
-    idMagazynu = models.IntegerField(default=0)
+class Magazyn(models.Model):
+    idMagazynu = models.IntegerField(default=0, primary_key=True)
+    nrOpakowania = models.ForeignKey(Opakowanie, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.idMagazynu
